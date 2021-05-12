90f9651da6b6df8807de0592e64a444cc4744944
Thomas Heck <t@b128.net>
2021-05-12 19:31:18 +0200

    build: add "yarn" nix shell dep

--- shell.nix
+++ shell.nix
@@ -1,9 +1,14 @@
 let
     pkgs = import <nixpkgs> {};
+    nodejs = pkgs.nodejs-14_x;
+    nodejsPkgs = pkgs.nodePackages.override {
+        nodejs = nodejs;
+    };
 in
     pkgs.stdenv.mkDerivation {
         name = "sketch.d.ts";
         buildInputs = [
-            pkgs.nodejs-14_x
+            nodejs
+            nodejsPkgs.yarn
         ];
     }
